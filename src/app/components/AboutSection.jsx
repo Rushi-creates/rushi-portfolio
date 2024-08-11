c0cf730e2cf2adbb64e330736a4bfead3ae791bc
rushi-creates <rushi.creates@gmail.com>
2024-08-11 07:39:13 +0530

    removed apostrophe

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -24,7 +24,7 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Master's of Computer Application</li>
+        <li>Master&apos;s of Computer Application</li>
         <li>DES NMITD, Mumbai, India</li>
       </ul>
     ),
@@ -51,10 +51,11 @@ const AboutSection = () => {
     });
   };
 
+
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/about-image.png" width={500} height={500} alt="about image" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
